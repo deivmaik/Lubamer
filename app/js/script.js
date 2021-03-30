908c31beb2c86e8f2cc060a71943d308b24f0ff1
unknown <77588948+deivmaik@users.noreply.github.com>
2021-03-30 23:53:55 +0200

    added mobile menu overlay disappear on click

--- app/js/script.js
+++ app/js/script.js
@@ -6,6 +6,7 @@ const body = document.querySelector('body')
 const header = document.querySelector('.header')
 const overlay = document.querySelector('.overlay')
 const fadeElements = document.querySelectorAll('.has-fade')
+const menuLinks = document.querySelectorAll('.menu-link')
 
 btnHamburger.addEventListener('click', function () {
 
@@ -28,6 +29,23 @@ btnHamburger.addEventListener('click', function () {
         })
     }
 })
+
+
+menuLinks.forEach((link)=>{
+    link.addEventListener('click', function () {
+        if (header.classList.contains('open')) {
+            // This closes the hamburger menu 
+            body.classList.remove('noscroll')
+            header.classList.remove('open')
+            fadeElements.forEach(function (element) {
+                element.classList.remove('fade-in')
+                element.classList.add('fade-out')
+            })
+        }
+    })
+
+})
+
 
 // animation for hamburger menu
 
